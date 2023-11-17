893ef79554dea3de8896fac714e693e3dfdfe485
irtaza-naveed <irtaza@agilekode.com>
2023-11-17 10:09:07 +0500

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Artist.create(name: "irtaza", age: 200)
+Artist.create(name: "ali", age: 200)
+Artist.create(name: "adeel", age: 200)
+Artist.create(name: "aqeel", age: 200)
+Artist.create(name: "saad", age: 200)
+Artist.create(name: "hello", age: 200)
